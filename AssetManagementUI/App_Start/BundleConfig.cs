dbcf64e5480925a9ff3e2700bef1a45533840c86
forhadparvez <forhadparvezfahim@gmail.com>
2017-07-16 17:12:00 +0600

    "Create Bundle"

---
+++ AssetManagementUI/App_Start/BundleConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace AssetManagementUI
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~bundles/bootstrap").Include(
+                "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/bootstrap.css",
+                "~/Content/site.css"));
+        }
+    }
+}
