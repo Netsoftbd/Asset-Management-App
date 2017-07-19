e6801b1804c43dae4c2589f55d4cf5fe4797d425
forhadparvez <forhadparvezfahim@gmail.com>
2017-07-19 19:10:31 +0600

    "Add Top Nav bar and Bundle"

--- AssetManagementUI/App_Start/BundleConfig.cs
+++ AssetManagementUI/App_Start/BundleConfig.cs
@@ -10,15 +10,35 @@ namespace AssetManagementUI
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                "~/Scripts/jquery-{version}.js"));
+            // Script Bundle
+            // base design header script
+            bundles.Add(new ScriptBundle("~/bundles/head").Include(
+                "~/BaseDesignAssets/js/ace-extra.min.js"));
 
-            bundles.Add(new ScriptBundle("~bundles/bootstrap").Include(
-                "~/Scripts/bootstrap.js"));
+            // base design footer script
+            bundles.Add(new ScriptBundle("~/bundle/jquery").Include(
+                "~/BaseDesignAssets/js/jquery-2.1.4.min.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                "~/BaseDesignAssets/js/bootstrap.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/ace").Include(
+                "~/BaseDesignAssets/js/ace-elements.min.js",
+                "~/BaseDesignAssets/js/ace.min.js"));
+
+
+
+            // Css Bundle
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                "~/Content/bootstrap.css",
                 "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/BaseDesign/css").Include(
+                "~/BaseDesignAssets/css/bootstrap.min.css",
+                "~/BaseDesignAssets/font-awesome/4.5.0/css/font-awesome.min.css",
+                "~/BaseDesignAssets/css/fonts.googleapis.com.css",
+                "~/BaseDesignAssets/css/ace.min.css",
+                "~/BaseDesignAssets/css/ace-skins.min.css",
+                "~/BaseDesignAssets/css/ace-rtl.min.css"));
         }
     }
 }
